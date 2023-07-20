24565692891a2d49a77e466e029a1b281aaf7261
ticoniq_ <okolietochi@gmail>
2023-07-20 16:55:24 +0100

    Create functions to display, add and remove date from api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,32 @@
-// import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getBookItems } from '../redux/books/booksSlice';
 import Card from '../components/Card';
 import AddBook from '../components/AddBook';
 
 function Home() {
-  const { bookItems, count } = useSelector((book) => book.bookItems);
-  if (count < 1) {
-    return (
-      <>
-        <div className="h-screen">
-          <p className="text-center ">Empty</p>
-        </div>
-        <AddBook />
-      </>
-    );
-  }
+  const List = useSelector((state) => state.bookItems.books);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBookItems());
+  }, [dispatch]);
 
   return (
     <>
-      {bookItems.map((item) => (
+      { List ? List.map((item) => (
         <Card
-          key={item.id}
-          id={item.id}
           title={item.title}
+          key={item.item_id}
+          id={item.item_id}
           author={item.author}
           category={item.category}
         />
-      ))}
+      )) : (
+        <div>
+          <h3>No Books</h3>
+        </div>
+      )}
       <div className="border-b-2 border-b-gray-200 pt-5" />
       <AddBook />
     </>
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,29 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+// const appId = 'Pv07lkhuAJvBV0UHCald';
+const appId = '8jyxEDElW0vb1k0o9GE1';
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps';
 
 const initialState = {
-  bookItems: [
-    {
-      id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
-  count: 4,
+  bookItems: [],
   isloading: true,
 };
+
+export const getBookItems = createAsyncThunk('books/getBookItems', async (thunkAPI) => {
+  try {
+    const response = await axios(`${url}/${appId}/books`);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('something went wrong');
+  }
+});
+
+export const addBookItem = createAsyncThunk('books/addNewBook', async (data, thunkAPI) => {
+  try {
+    const response = await axios.post(`${url}/${appId}/books`, data);
+    thunkAPI.dispatch(getBookItems());
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('something went wrong');
+  }
+});
+
+export const removeBookItem = createAsyncThunk('books/removeBookItem', async (id, thunkAPI) => {
+  try {
+    const response = await axios.delete(`${url}/${appId}/books/${id}`, id);
+    thunkAPI.dispatch(getBookItems());
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('something went wrong');
+  }
+});
 
 const bookSlice = createSlice({
   name: 'books',
@@ -36,6 +50,48 @@ const bookSlice = createSlice({
       const itemId = action.payload;
       state.bookItems = state.bookItems.filter((item) => item.id !== itemId);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getBookItems.pending, (state) => {
+        state.isloading = true;
+      })
+      .addCase(getBookItems.fulfilled, (state, action) => {
+        state.isloading = false;
+        const data = Object.keys(action.payload).map((item) => {
+          const book = {};
+          book.author = action.payload[item][0].author;
+          book.category = action.payload[item][0].category;
+          book.title = action.payload[item][0].title;
+          book.item_id = item;
+          return book;
+        });
+        state.books = data;
+      })
+      .addCase(getBookItems.rejected, (state, action) => {
+        state.isloading = false;
+        state.error = action.payload;
+      })
+      .addCase(addBookItem.pending, (state) => {
+        state.isloading = true;
+      })
+      .addCase(addBookItem.fulfilled, (state) => {
+        state.isloading = false;
+      })
+      .addCase(addBookItem.rejected, (state, action) => {
+        state.isloading = false;
+        state.error = action.payload;
+      })
+      .addCase(removeBookItem.pending, (state) => {
+        state.isloading = true;
+      })
+      .addCase(removeBookItem.fulfilled, (state) => {
+        state.isloading = false;
+      })
+      .addCase(removeBookItem.rejected, (state, action) => {
+        state.isloading = false;
+        state.error = action.payload;
+      });
   },
 });
 
